6aa32221c79df29f0e84f1be49033e534324fcc5
Marko-S-O <rek@orasaari.com>
2024-10-16 23:00:19 +0300

    Add files via upload

--- src/modelling/revenue_forecast_multicaptive_2.py
+++ src/modelling/revenue_forecast_multicaptive_2.py
@@ -19,11 +19,11 @@ class RevenueForecastMulticaptive2:
     # Initialize the model. 
     def __init__(self):
 
-        self.data = ForecastUtils.load_data()
+        self.data = ForecastUtils.load_rate_normalized_data()
         self.data = self.data.rename(columns={'Unnamed: 0': 'Month_index'})
 
         # Select relevant columns and drop na
-        self.data_models = self.data[['Month_index', 'Revenue FIN', 'Revenue IND', 'Revenue NS']]
+        self.data_models = self.data[['Month_index', 'D/M FIN', 'Revenue FIN', 'Revenue IND', 'Revenue NS']]
         self.data_models = self.data_models.dropna(subset = ['Revenue FIN'])
         self.data_models = self.data_models.iloc[:-1]        
         
@@ -41,31 +41,48 @@ class RevenueForecastMulticaptive2:
         # This is the actual used training data
         training_data = ForecastUtils.split_data(self.data_models, train_start, train_end)
         #training_data = self.data_models[:-6] - if this would be used, 10/24 should be manually dropped
-       
+
+        # Include working days in the training data
+        training_data['Working_days_FIN'] = self.train_data['D/M FIN']
+        training_data['Working_days_IND'] = self.train_data['D/M IND']
+        training_data['Working_days_NS'] = self.train_data['D/M NS']
+
+        # Normalize revenue by working days for each revenue type
+        training_data['Revenue_FIN_normalized'] = training_data['Revenue FIN'] / training_data['Working_days_FIN']
+        training_data['Revenue_IND_normalized'] = training_data['Revenue IND'] / training_data['Working_days_IND']
+        training_data['Revenue_NS_normalized'] = training_data['Revenue NS'] / training_data['Working_days_NS']
+
         # Get the columns for each model in a separate dataframe
-        data_FIN = training_data[['Month_index', 'Revenue FIN']]
-        data_IND = training_data[['Month_index', 'Revenue IND']]
-        data_NS = training_data[['Month_index', 'Revenue NS']]
-        data_FIN = data_FIN.set_index('Month_index')
-        data_IND = data_IND.set_index('Month_index')
-        data_NS = data_NS.set_index('Month_index')
+        #data_FIN = training_data[['Month_index', 'Revenue FIN']]
+        #data_IND = training_data[['Month_index', 'Revenue IND']]
+        #data_NS = training_data[['Month_index', 'Revenue NS']]
+        #data_FIN = data_FIN.set_index('Month_index')
+        #data_IND = data_IND.set_index('Month_index')
+        #data_NS = data_NS.set_index('Month_index')
+
+        # Set index for each revenue type
+        data_FIN = training_data[['Month_index', 'Revenue_FIN_normalized']].set_index('Month_index')
+        data_IND = training_data[['Month_index', 'Revenue_IND_normalized']].set_index('Month_index')
+        data_NS = training_data[['Month_index', 'Revenue_NS_normalized']].set_index('Month_index')
+
         data_FIN_train = data_FIN
         data_IND_train = data_IND
         data_NS_train = data_NS
 
         # Create models
-        self.multiplicative_model_FIN = seasonal_decompose(data_FIN_train['Revenue FIN'], model='multiplicative', period=12)
-        self.multiplicative_model_IND = seasonal_decompose(data_IND_train['Revenue IND'], model='multiplicative', period=12)
-        self.multiplicative_model_NS = seasonal_decompose(data_NS_train['Revenue NS'], model='multiplicative', period=12)
+        #self.multiplicative_model_FIN = seasonal_decompose(data_FIN_train['Revenue FIN'], model='multiplicative', period=12)
+        #self.multiplicative_model_IND = seasonal_decompose(data_IND_train['Revenue IND'], model='multiplicative', period=12)
+        #self.multiplicative_model_NS = seasonal_decompose(data_NS_train['Revenue NS'], model='multiplicative', period=12)
+
+        # Seasonal decomposition on normalized revenue
+        self.multiplicative_model_FIN = seasonal_decompose(data_FIN['Revenue_FIN_normalized'], model='multiplicative', period=12)
+        self.multiplicative_model_IND = seasonal_decompose(data_IND['Revenue_IND_normalized'], model='multiplicative', period=12)
+        self.multiplicative_model_NS = seasonal_decompose(data_NS['Revenue_NS_normalized'], model='multiplicative', period=12)
+
 
         # Create a vector for monthly multipliers for FIN
         # Create a list of month names
         months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-
-        # Define the seasonality based on all full years found in the train data
-        first_year = self.train_data.iloc[0]['Year'] if (self.train_data.iloc[0]['Month']==1) else self.train_data.iloc[0]['Year']+1
-        last_year = self.train_data.iloc[-1]['Year'] if (self.train_data.iloc[-1]['Month']==12) else self.train_data.iloc[0]['Year']-1
-        full_years = self.train_data[(self.train_data['Year'] >= first_year) & (self.train_data['Year'] <= last_year)]
 
         # Calculate the monthly average for each year
         # Group by 'Year' and 'Month', and then calculate the mean for each month across all full years
@@ -106,6 +123,11 @@ class RevenueForecastMulticaptive2:
 
         forecast_data = ForecastUtils.split_data(self.data, forecast_start, forecast_end)
 
+        # Include working days in the forecast data
+        forecast_data['Working_days_FIN'] = forecast_data['D/M FIN']
+        forecast_data['Working_days_IND'] = forecast_data['D/M IND']
+        forecast_data['Working_days_NS'] = forecast_data['D/M NS']
+
         #print(forecast_data)
         slope_fin, intercept_fin, r_value_fin, p_value_fin, std_err_fin = linregress(self.cleaned_trend_FIN.index, self.cleaned_trend_FIN.values)
         slope_ind, intercept_ind, r_value_ind, p_value_ind, std_err_ind = linregress(self.cleaned_trend_IND.index, self.cleaned_trend_IND.values)
@@ -128,6 +150,7 @@ class RevenueForecastMulticaptive2:
         multis_per_predicted_month_ns = self.monthly_multi_NS.iloc[
             [forecast_data.iloc[i]['Month'] - 1 for i in range(len(forecast_data))]
         ]
+
         # Seasonal components (we are predicting 4/24 - 9/24 so we take monthly multipliers indexed 3 to 8)
         # Multiplier can be found from vector monthly_multi_*
         #multis_per_predicted_month_fin = self.monthly_multi_FIN[3:9]        
@@ -150,6 +173,11 @@ class RevenueForecastMulticaptive2:
             'Predictions': [(slope_ns * x + intercept_ns) * multis_per_predicted_month_ns.values[i] [1]
                         for i, x in enumerate(months_to_predict)]
         })
+
+        # Scale predictions by working days
+        predictions_df_FIN['Predictions'] *= forecast_data['Working_days_FIN'].values
+        predictions_df_IND['Predictions'] *= forecast_data['Working_days_IND'].values
+        predictions_df_NS['Predictions'] *= forecast_data['Working_days_NS'].values
 
         # Set index same for both so that we can sum these things
         #if 'Month_index' in predictions_df_FIN.columns:
